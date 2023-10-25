d3bd64ff1350414fbfe7750b04ceea73ed92ec3c
Daniel B <daniel.m.bassett@outlook.com>
2023-10-25 05:17:58 -0500

    made graphs fit container width

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
 import plotly_express as px
 import streamlit as st
 from streamlit_option_menu import option_menu
@@ -31,44 +33,22 @@ if selected == 'Overall':
     metro_options = ['All'] + sorted(list(merged_df['metro'].unique()))
     metro = st.selectbox('Choose Metro', options=metro_options, index=0)
 
-    # line chart
+    # bar chart
     if metro != 'All':
-        ratings_compare = merged_df.query(f'metro == "{metro}"').groupby(['year', 'bank'], as_index=False)['stars'].mean().sort_values(by=['bank', 'year'],ascending=False).reset_index(drop=True)
+        ratings_compare = merged_df.query(f'metro == "{metro}"').groupby(['bank'])['stars'].mean().sort_values(ascending=False)
     else:
-        ratings_compare = merged_df.groupby(['year', 'bank'], as_index=False)['stars'].mean().sort_values(by=['bank', 'year'],ascending=False).reset_index(drop=True)
-
-    fig = px.line(
+        ratings_compare = merged_df.groupby('bank')['stars'].mean().sort_values(ascending=False)
+    fig = px.bar(
         data_frame=ratings_compare,
-        title=f'Average Ratings - {metro}',
-        x='year',
-        y='stars',
-        color='bank'
+        title=f'Average Ratings - {metro}'
     )
     fig.update_layout(
         yaxis_title="stars", 
         yaxis=dict(range=[0, 5]),
         title_x=0.4,
-        showlegend=True
+        showlegend=False
         )
-    st.plotly_chart(fig)
-
-
-    # bar chart
-    # if metro != 'All':
-    #     ratings_compare = merged_df.query(f'metro == "{metro}"').groupby(['bank'])['stars'].mean().sort_values(ascending=False)
-    # else:
-    #     ratings_compare = merged_df.groupby('bank')['stars'].mean().sort_values(ascending=False)
-    # fig = px.bar(
-    #     data_frame=ratings_compare,
-    #     title=f'Average Ratings - {metro}'
-    # )
-    # fig.update_layout(
-    #     yaxis_title="stars", 
-    #     yaxis=dict(range=[0, 5]),
-    #     title_x=0.4,
-    #     showlegend=False
-    #     )
-    # st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 
     # pie chart
@@ -79,27 +59,21 @@ if selected == 'Overall':
         n_branches = branches_df[metro_filter].groupby('bank', as_index=False).size().sort_values(by='bank')
 
     n_branches = n_branches.rename(columns={'size': 'n_branches'})
+    
     fig = px.pie(n_branches, values='n_branches', names='bank', title=f'Market Share - {metro}')
     fig.update_layout(
         title_x=0.4,
         margin=dict(t=50, b=50, l=200, r=50)
         )
-    st.plotly_chart(fig)
+    st.plotly_chart(fig, use_container_width=True)
 
 
 
 
 if selected == 'Branch Report':
-    # metro_options = ['All'] + sorted(list(merged_df['metro'].unique()))
-    # metro = st.selectbox('Choose Metro', options=metro_options, index=0)
 
     merged_df = pd.merge(reviews_df, branches_df, left_on='branch_id', right_on='branch_id')
     branches_filtered = branches_df.copy()
-
-    # metro filter
-    # metro_filter = branches_filtered['metro'] == metro
-    # if metro != 'All':
-    #     branches_filtered = branches_filtered[metro_filter]
 
     # bank filter
     bank_options = branches_filtered['bank'].unique()
@@ -115,9 +89,24 @@ if selected == 'Branch Report':
 
     # write report
     if len(branches_filtered) == 1:
+        id_filter = reviews_df['branch_id'] == branches_filtered.branch_id.values[0]
+
+        branch_ratings = merged_df[id_filter][['stars', 'year']]
+        branch_ratings = branch_ratings.groupby('year')['stars'].mean()
+        
+        fig = px.line(
+            data_frame=branch_ratings, title='Average Rating'
+        )
+        fig.update_layout(
+            yaxis_title="stars", 
+            yaxis=dict(range=[0, 5]),
+            title_x=0.4,
+            showlegend=True
+            )
+        st.plotly_chart(fig, use_container_width=True)
+
         st.write(branches_filtered['analysis'].values[0])
 
         with st.expander('See Reviews...'):
-            id_filter = reviews_df['branch_id'] == branches_filtered.branch_id.values[0]
             for index, row in reviews_df[id_filter].dropna().iterrows():
                 st.write(row['review'])
